123defeeac9b901399b428da67dabd8eb5e7bc84
Franklyn Reyes <franklynreyes777@yahoo.com>
2024-10-15 15:08:12 -0400

    initial project

---
+++ myfirstserverapp/src/main/java/com/mybackendhelloworld/projectserver/demo/myfirstserverapp/MyfirstserverappApplication.java
@@ -0,0 +1,13 @@
+package com.mybackendhelloworld.projectserver.demo.myfirstserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstserverappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstserverappApplication.class, args);
+	}
+
+}
---
+++ myfirstserverapp/src/test/java/com/mybackendhelloworld/projectserver/demo/myfirstserverapp/MyfirstserverappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybackendhelloworld.projectserver.demo.myfirstserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstserverappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
